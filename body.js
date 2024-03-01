81d732e469524e16609a99d051d800dc7c977b07
Manuel J <59123244+PianoManu@users.noreply.github.com>
2024-03-01 21:26:20 +0100

    Removed empty app.js

--- body.js
+++ body.js
@@ -8,6 +8,5 @@ const index_body = document.querySelector(".loadscripts");
 const index_body_new_item = document.createElement("div");
 index_body_new_item.innerHTML=`
 <script src="navigator.js"></script>
-<script src="app.js"></script>
 `;
 index_body.appendChild(index_body_new_item);
