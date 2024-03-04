acce76f2c1fb0957aff0ce0a4c9ac12554b741bd
Manuel J <59123244+PianoManu@users.noreply.github.com>
2024-03-04 21:52:10 +0100

    Extracted impressum & moved to impressum.js for easier processing, removed low-level header.js and replaced links with top-level header.js

--- header.js
+++ header.js
@@ -1,11 +1,19 @@
-const template = document.createElement('template')
+console.log(`Loading header.js for file "${document.URL}..."`)
+let hierarchy_level = "";
+if (document.URL.includes("content")) {
+    hierarchy_level = "../";
+}
+if (document.URL.includes("beliefs")) {
+    hierarchy_level = "../../";
+}
+template = document.createElement('template')
 template.innerHTML=`
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="description" content="Internetseite von bibeltreuen Christen in Ilmenau ohne besondere Konfession">
 <meta name="keywords" content="Christ, Christen, Ilmenau, Gemeinde, Versammlung, bibeltreu, Bibel, Kirche">
 <meta name="author" content="Christen in Ilmenau">
-<link rel="stylesheet" href="basestyles.css" />
+<link rel="stylesheet" href="${hierarchy_level}basestyles.css" />
 <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed" rel="stylesheet">
 <script src="https://kit.fontawesome.com/d6281656fa.js" crossorigin="anonymous"></script>
 `
---
+++ impressum.js
@@ -0,0 +1,11 @@
+let impressum = `Verantwortlich für den Inhalt:
+<br>
+<br>
+Christen in Ilmenau
+<br>
+Mühlenstraße 9
+<br>
+98693 Ilmenau
+<br>
+Mail: <b><i>TODO</i></b>`;
+document.getElementById("impressum").innerHTML = impressum;
