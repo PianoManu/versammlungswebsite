58b8560ebdc2b5510080849b4faf1e5de6856c12
Manuel J <59123244+PianoManu@users.noreply.github.com>
2024-04-27 17:22:52 +0200

    Fixed banner, added to all pages

--- body.js
+++ body.js
@@ -4,7 +4,20 @@ const index_body_new_item = document.createElement("div");
 index_body_new_item.innerHTML=`
 <script src="navigator.js"></script>
 `;
-index_body.appendChild(index_body_new_item);
+//index_body.appendChild(index_body_new_item);
+let hierarchy_level_body = "";
+if (document.URL.includes("content")) {
+    hierarchy_level_body = "../";
+}
+if (document.URL.includes("beliefs")) {
+    hierarchy_level_body = "../../";
+}
+template = document.createElement('template');
+template.innerHTML = `
+<img src="${hierarchy_level_body}/img/photos/edited/IMG20230615122424.svg" class="banner">
+<hr class="hr_brown">
+`
+document.body.prepend(template.content);
 
 function openDropdown() {
     document.getElementById("myDropdown").classList.toggle("show");
