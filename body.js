0555740a6ae435e4ac8afede04e2a31099e6319e
Manuel J <59123244+PianoManu@users.noreply.github.com>
2024-04-27 17:24:14 +0200

    Cleaned up JS

--- body.js
+++ body.js
@@ -1,10 +1,4 @@
 console.log(`Loading ./body.js for ${document.URL}`)
-const index_body = document.querySelector(".loadscripts");
-const index_body_new_item = document.createElement("div");
-index_body_new_item.innerHTML=`
-<script src="navigator.js"></script>
-`;
-//index_body.appendChild(index_body_new_item);
 let hierarchy_level_body = "";
 if (document.URL.includes("content")) {
     hierarchy_level_body = "../";
