4a32a2ec747704f6c84f255776a495ca9db277f3
Manuel J <59123244+PianoManu@users.noreply.github.com>
2024-04-27 22:06:08 +0200

    Added slideshow

---
+++ slide_show.js
@@ -0,0 +1,29 @@
+console.log(`Loading slide_show.js for file "${document.URL}..."`)
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("slides_default");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+        slides[i].style.position = "relative";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    }  
+    let bot_img = slideIndex-1 > 0 ? slideIndex-2: slides.length-1;
+
+    // top img
+    slides[slideIndex-1].style.display = "block";
+    slides[slideIndex-1].style.zIndex = 2;
+    slides[slideIndex-1].style.position = "relative";
+
+    // bottom img
+    slides[bot_img].style.display = "block";
+    slides[bot_img].style.top = "0%";
+    slides[bot_img].style.position = "absolute";
+    slides[bot_img].style.zIndex = 1;
+    setTimeout(showSlides, 10000); // Change image every 10 seconds
+}
