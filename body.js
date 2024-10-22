53a2d99fab3a7c338a06874852ff61bff2594349
Manuel J <59123244+PianoManu@users.noreply.github.com>
2024-10-22 13:18:21 +0200

    Removed page banner since it takes up too much space and resources

--- body.js
+++ body.js
@@ -6,12 +6,6 @@ if (document.URL.includes("content")) {
 if (document.URL.includes("beliefs")) {
     hierarchy_level_body = "../../";
 }
-template = document.createElement('template');
-template.innerHTML = `
-<img src="${hierarchy_level_body}img/photos/edited/IMG20230615122424.png" class="banner">
-<hr class="hr_brown">
-`
-document.body.prepend(template.content);
 
 function openDropdown() {
     document.getElementById("myDropdown").classList.toggle("show");
